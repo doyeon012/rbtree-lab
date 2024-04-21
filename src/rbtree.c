6ffc06b5c4cfbb6a671bbcd18322dbe0ed1b73c0
doyeon012 <kim2004on@gmail.com>
2024-04-21 20:47:57 +0900

    트리생성, 삭제 구현

--- src/rbtree.c
+++ src/rbtree.c
@@ -2,42 +2,79 @@
 
 #include <stdlib.h>
 
+//메모리 : 새트리 생성
 rbtree *new_rbtree(void) {
-  rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
-  // TODO: initialize struct if needed
-  return p;
+
+// tree 구조체 동적 할당
+rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
+
+// nil 노드 생성 및 초기화
+node_t *nil = (node_t *)calloc(1, sizeof(node_t));
+nil->color = RBTREE_BLACK;
+
+// tree의 nil과 root를 nil 노드로 설정(tree가 비었어? > root는 nil 노드여야 한다.)
+p->nil = nil;
+p->root = nil;
+
+return p;
 }
 
+// 트리 삭제
 void delete_rbtree(rbtree *t) {
-  // TODO: reclaim the tree nodes's memory
+  node_t * node = t->root;
+
+  //트리를 순회하면서 각 노드의 메모리를 반환하는 함수
+  if (node != t->nil)
+  {
+    free_node(t, node);
+  }
+
+  //nil 노드와 rbtree 구조체의 메모리를 반환
+  free(t->nil);
   free(t);
 }
 
+//각 노드와 그 자식 노드들의 메모리를 반환하는 함수, 후위 순회방식
+void free_node(rbtree *t, node_t *node)
+{
+  if(node->left != t->nil)
+    free_node(t, node->left);
+
+  if(node->right != t->nil)
+    free_node(t, node->right);
+
+  //현재 노드의 메모리를 반환
+  free(node);
+  node = NULL;
+}
+
+
+// 노드 삽입
 node_t *rbtree_insert(rbtree *t, const key_t key) {
   // TODO: implement insert
   return t->root;
 }
 
+//노드 검색
 node_t *rbtree_find(const rbtree *t, const key_t key) {
   // TODO: implement find
   return t->root;
 }
-
 node_t *rbtree_min(const rbtree *t) {
   // TODO: implement find
   return t->root;
 }
-
 node_t *rbtree_max(const rbtree *t) {
   // TODO: implement find
   return t->root;
 }
 
+//노드 삭제
 int rbtree_erase(rbtree *t, node_t *p) {
   // TODO: implement erase
   return 0;
 }
-
+// 트리를 배열로 변환 > 중위 순회로 구현
 int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
   // TODO: implement to_array
   return 0;
--- src/rbtree.h
+++ src/rbtree.h
@@ -3,30 +3,35 @@
 
 #include <stddef.h>
 
-typedef enum { RBTREE_RED, RBTREE_BLACK } color_t;
+typedef enum { RBTREE_RED, RBTREE_BLACK } color_t; // 열거형
 
-typedef int key_t;
+typedef int key_t; // 각 노드에 저장되는 키 값을 나타내는 데이터 형식
 
+
+// tree의 각 노드를 표현하는 구조체
 typedef struct node_t {
   color_t color;
   key_t key;
   struct node_t *parent, *left, *right;
 } node_t;
 
+// tree 자체를 나타내는 구조
 typedef struct {
-  node_t *root;
-  node_t *nil;  // for sentinel
+  node_t *root; // 루트노드
+  node_t *nil;  // nil
 } rbtree;
 
-rbtree *new_rbtree(void);
-void delete_rbtree(rbtree *);
+rbtree *new_rbtree(void); // 새로운 RB트리 생성 + 반환
+void delete_rbtree(rbtree *); //RB 트리 삭제
 
-node_t *rbtree_insert(rbtree *, const key_t);
-node_t *rbtree_find(const rbtree *, const key_t);
-node_t *rbtree_min(const rbtree *);
-node_t *rbtree_max(const rbtree *);
-int rbtree_erase(rbtree *, node_t *);
+node_t *rbtree_insert(rbtree *, const key_t);//노드 삽입
 
-int rbtree_to_array(const rbtree *, key_t *, const size_t);
+node_t *rbtree_find(const rbtree *, const key_t);// 주어진 키 값을 가진 노드 반환
+node_t *rbtree_min(const rbtree *); // 가장 작은 키 값을 가진 노드 반환
+node_t *rbtree_max(const rbtree *);// 가장 큰 키 값을 가진 노드를 반환.
+
+int rbtree_erase(rbtree *, node_t *); // 노드 삭제
+
+int rbtree_to_array(const rbtree *, key_t *, const size_t); // RB 트리에 모든 키 값을 배열로 변환
 
 #endif  // _RBTREE_H_
